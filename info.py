1067d92caa31b6df86c44d1c73f83c9324a7a7e8
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2024-12-29 14:00:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,12 +72,12 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 
 # Links
-SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/HA_Bots_Support')
-OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/Hansaka_Anuhas")
-UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/HA_Bots')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/HA_Films_World')
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/HA_Bots")
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/HA_Bots")
+SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/MoviesSearchProGroup')
+OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/Satyajeetkumarofficial")
+UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/MovieSearchProSupport')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/MoviesSearchPro')
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/MovieSearchProSupport")
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/MovieSearchProSupport")
 
 # Bot settings
 DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds
