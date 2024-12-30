f8de08c677b5a9c2d39f518f5425d4d32c75fb9d
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2024-12-30 07:07:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ uvloop.install()
 class Bot(Client):
     def __init__(self):
         super().__init__(
-            name='Movie_Search_Pro',
+            name='Auto_Filter_Bot',
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
